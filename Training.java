d9840e04dedab8487ab0b189117bec96e1490699
tavisca-ragupta <ragupta@tavisca.in>
2019-06-20 16:23:14 +0530

    Modified

--- Training.java
+++ Training.java
@@ -5,5 +5,6 @@ public class FirstTrainingProject
  public static void main(String...args)
 {
  System.out.print("Training Started.....");
+System.out.print("IT IS DUPLICATE FILE IN GREET BRANCH");
 }
 }
